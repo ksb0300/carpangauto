d24a3c206c819ec4736215075ffba7f75b3e6065
carpang <ksb0300@gmail.com>
2026-08-26 14:58:04 +0900

    매장 전화번호 반영 (051-711-7818 / 010-7683-5388)
    푸터·오시는길에 두 번호를 넣고, 홈과 오시는길에 바로 거는 버튼을 붙였다.
    번호는 assets/site.js 의 매장 객체 한 곳에서만 관리한다.

--- assets/site.js
+++ assets/site.js
@@ -4,7 +4,8 @@ var 매장 = {
   이름: "카팡모터스",
   주소: "부산광역시 금정구 반송로 425, 3층 311호",
   대표: "김상혁",
-  전화: "",              // ← 매장 대표번호를 넣으면 화면 곳곳에 반영된다
+  전화: "051-711-7818",     // 매장 대표번호
+  휴대폰: "010-7683-5388",   // 상담 휴대폰
   영업: "매일 09:00 – 19:00"
 };
 
@@ -15,6 +16,10 @@ var 메뉴 = [
   ["/sign/",    "서류 작성"],
   ["/contact/", "오시는 길"]
 ];
+
+function 전화링크(번호) {
+  return '<a href="tel:' + 번호.replace(/[^0-9]/g, "") + '">' + 번호 + "</a>";
+}
 
 (function () {
   var here = location.pathname.replace(/index\.html$/, "");
@@ -31,10 +36,11 @@ var 메뉴 = [
     '<nav class="nav">' + nav + "</nav>" +
     "</div></header>");
 
-  var 전화줄 = 매장.전화
-    ? '<p style="margin:0 0 4px">전화 <a href="tel:' +
-      매장.전화.replace(/[^0-9]/g, "") + '">' + 매장.전화 + "</a></p>"
-    : "";
+  var 연락 = [];
+  if (매장.전화)   연락.push(전화링크(매장.전화));
+  if (매장.휴대폰) 연락.push(전화링크(매장.휴대폰));
+  var 전화줄 = 연락.length
+    ? '<p style="margin:0 0 4px">' + 연락.join(" &nbsp;·&nbsp; ") + "</p>" : "";
 
   document.body.insertAdjacentHTML("beforeend",
     '<footer class="ft"><div class="wrap">' +
